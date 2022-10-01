d5a1647ff9dfc7a71e85e9b75fc5b5bef76b60cf
Kunal Shrivastva <88685195+kunalsrv20@users.noreply.github.com>
2022-10-01 21:28:01 +0530

    Add files via upload

---
+++ Codebook/Data-Structures/treapLazy.cpp
@@ -0,0 +1,93 @@
+struct Node {
+    char val;
+    int weight, size;
+    Node *left, *right;
+    int toinvert;
+    Node(char c) : val(c), weight(rand()), size(1), left(NULL), right(NULL), toinvert(0) {}
+} *root;
+ 
+inline int size(Node *treap) {
+    return treap ? treap->size : 0;
+}
+ 
+void push(Node *treap) {
+    if(treap == NULL) return;
+    if(treap->toinvert == 0) return;
+ 
+    Node *temp = treap->left;
+    treap->left = treap->right;
+    treap->right = temp;
+    treap->toinvert = 0;
+ 
+    if(treap->left != NULL) treap->left->toinvert ^= 1;
+    if(treap->right != NULL) treap->right->toinvert ^= 1;
+}
+ 
+void split(Node *treap, Node *&left, Node *&right, int val) {
+    if (!treap) {
+        left = right = NULL;
+        return;
+    }
+    push(treap);
+    if (size(treap->left) < val) {
+        split(treap->right, treap->right, right, val - size(treap->left) - 1);
+        left = treap;
+    } else {
+        split(treap->left, left, treap->left, val);
+        right = treap;
+    }
+    treap->size = 1 + size(treap->left) + size(treap->right);
+}
+ 
+void merge(Node *&treap, Node *left, Node *right) {
+    if (left == NULL) {
+        treap = right;
+        return;
+    }
+    if (right == NULL) {
+        treap = left;
+        return;
+    }
+    push(left);
+    push(right);
+    if (left->weight < right->weight) {
+        merge(left->right, left->right, right);
+        treap = left;
+    } else {
+        merge(right->left, left, right->left);
+        treap = right;
+    }
+    treap->size = 1 + size(treap->left) + size(treap->right);
+}
+ 
+ostream& operator<<(ostream &os, Node *n) {
+    if (!n) return os;
+    push(n);
+    os << n->left;
+    os << n->val;
+    os << n->right;
+    return os;
+}
+ 
+void solve() {  
+ 
+    int n, q;
+    string s;
+    cin >> n >> q;
+    cin >> s;
+    for(auto c: s) {
+        merge(root, root, new Node(c));
+    }
+    while(q--) {
+        int l, r;
+        cin >> l >> r;
+        Node *a, *b;
+        split(root, a, b, l - 1);
+        Node *c, *d;
+        split(b, c, d, r - l + 1);
+        c->toinvert ^= 1;
+        merge(root, a, c);
+        merge(root, root, d);
+    }
+    cout << root << nl;
+}
