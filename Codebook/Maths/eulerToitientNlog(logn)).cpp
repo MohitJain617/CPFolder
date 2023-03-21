7cc0c394a2dbb1a852de6f9fbb6fc53e1bee6fd6
Mohit Jain <83278247+MohitJain617@users.noreply.github.com>
2023-03-22 01:16:35 +0530

    Mobius+phi (n log log)

--- Codebook/Maths/eulerToitientNlog(logn)).cpp
+++ Codebook/Maths/eulerToitientNlog(logn)).cpp
@@ -1,10 +1,14 @@
-void phi_1_to_n(int n) {
+void mobiphin(int n) {
     vector<int> phi(n + 1);
-    for (int i = 0; i <= n; i++)
-     phi[i] = i;
+    vector<int> mu(n + 1);
+    for (int i = 0; i <= n; i++){
+     phi[i] = i; mu[i] = 1;
+    }
     for (int i = 2; i <= n; i++) {
      if (phi[i] == i) {
          for (int j = i; j <= n; j += i)
           phi[j] -= phi[j] / i;
+          if((j/i)%i == 0) mu[j] = 0;
+          mu[j] = -mu[j];
          }
     }}
