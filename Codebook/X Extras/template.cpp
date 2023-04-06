8815578c42d729fc7a639c6127208505d492ba3a
Kunal Shrivastva <88685195+kunalsrv20@users.noreply.github.com>
2023-04-07 02:38:03 +0530

    Update template.cpp

--- Codebook/X Extras/template.cpp
+++ Codebook/X Extras/template.cpp
@@ -1,18 +1,10 @@
-#include <bits/stdc++.h>
-using namespace std;
 #define rep(i, a, b) for(int i = a; i < (b); ++i)
-#define all(x) begin(x), end(x)
-#define sz(x) (int)(x).size()
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef vector<int> vi;
-#define MOD1 1000000007
-#define MOD2 1000000009
-#define MOD3 1000000021
-#define MOD4 1000000033
-#define MOD5 1000000087
-#define MOD6 1000000093
-#define MOD7 1000000097
+#define MOD2 1000000009 #define MOD3 1000000021
+#define MOD4 1000000033 #define MOD5 1000000087
+#define MOD6 1000000093 #define MOD7 1000000097
+const ll RANDOM = chrono::high_resolution_clock::now().time_since_epoch().count();
+struct chash { ll operator()(ll x) const { return x ^ RANDOM; } };
+gp_hash_table<ll, ll, chash> dp;
 int main(){
 	ios::sync_with_stdio(false);
 	cin.tie(0);
