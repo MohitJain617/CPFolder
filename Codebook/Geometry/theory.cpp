b4f58d84e6e039343c48e9ba0b40d033ec59c13b
Shivansh Choudhary <shivansh20127@iiitd.ac.in>
2023-04-07 14:01:12 +0530

    Create theory.cpp

---
+++ Codebook/Geometry/theory.cpp
@@ -0,0 +1,6 @@
+/*
+If you represent lines as equations like
+ax + by = c
+dx + ey = f
+then intersection is the solution of the equation system which you can find using Cramer's rule, which means that you need to calculate 3 2x2 determinants A, B, C,  and answers are A/C, B/C. Lines are parallel (or coincide) iff C=0
+*/
